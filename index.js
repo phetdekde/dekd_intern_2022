da55e21981f39b2296f50193b32d28bd64ffd09b
Jirawat Santichavalit <phetdekde@hotmail.com>
2022-01-28 19:23:35 +0700

    done

---
+++ index.js
@@ -0,0 +1,36 @@
+const back = document.getElementById('back');
+const next = document.getElementById('next');
+const imgList = document.getElementsByTagName('img');
+const dotList = document.querySelectorAll('.dot-items');
+let currentPage = 0;
+
+back.addEventListener('click', () => {
+    if(currentPage > 0) {
+        scrollTo(currentPage - 1);
+    } else {
+        scrollTo(imgList.length - 1);
+    }
+});
+
+next.addEventListener('click', () => {
+    if(currentPage < imgList.length - 1) {
+        scrollTo(currentPage + 1);
+    } else {
+        scrollTo(0);
+    }
+});
+
+for (let i = 0; i < dotList.length; i++) {
+    dotList[i].addEventListener('click', () => {
+        scrollTo(i);
+    });
+}
+
+const scrollTo = (selectedPage) => {
+    dotList[currentPage].classList.toggle('selected');
+    dotList[selectedPage].classList.toggle('selected');
+    currentPage = selectedPage;
+    imgList[selectedPage].scrollIntoView({
+        inline: 'center' 
+    });
+} 
